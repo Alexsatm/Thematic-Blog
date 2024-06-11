9b61290e64a7500a4d31721e39afb4cc6df6c2f2
Alexsatm <dzamilovaalbina2@gmail.com>
2024-06-12 01:06:11 +0300

    add tags block

--- src/components/AllArticles/AllArticles.tsx
+++ src/components/AllArticles/AllArticles.tsx
@@ -10,10 +10,6 @@ export const AllArticles = () => {
   return (
     <div>
       <div className={styles.top}>
-        <div className="heading-box">
-          <h2 className="heading">All Articles</h2>
-          <div className="heading-line"></div>
-        </div>
         <div className={styles.topRow}>
           <div className={styles.topBox}>
             <img src={newest1} alt="" />
@@ -22,7 +18,7 @@ export const AllArticles = () => {
               <span className="subtitle">NATURE</span>
               <h3 className="title">Tempor deserunt Sunt Qui</h3>
               <span className="meta">Connie Robertson November 3, 2012</span>
-              <p className="desc">
+              <p className={styles.desc}>
                 Ea qui dolor aute cupidatat ad pariatur proident. Mollit nulla tempor aute
                 reprehenderit ut dolore mollit nisi consequat excepteur ex officia pariatur irure.
               </p>
@@ -36,7 +32,7 @@ export const AllArticles = () => {
               <span className="subtitle">PLANTS</span>
               <h3 className="title">Occaecat Anim eu Qui</h3>
               <span className="meta">Eleanor Williamson September 28, 2014</span>
-              <p className="desc">
+              <p className={styles.desc}>
                 Qui ipsum consectetur ad incididunt et aliquip exercitation laboris nisi
                 reprehenderit. Et excepteur commodo esse enim ea aliqua officia reprehenderit.
               </p>
@@ -50,7 +46,7 @@ export const AllArticles = () => {
               <span className="subtitle">FOOD</span>
               <h3 className="title">Fugiat Incididunt Voluptate Consequat</h3>
               <span className="meta">Bessie Watson June 13, 2012</span>
-              <p className="desc">Mollit ea culpa ipsum pariatur eiusmod. Irure et.</p>
+              <p className={styles.desc}>Mollit ea culpa ipsum pariatur eiusmod. Irure et.</p>
             </div>
           </div>
           {/*  */}
@@ -61,7 +57,7 @@ export const AllArticles = () => {
               <span className="subtitle">COLORFUL</span>
               <h3 className="title">Occaecat Anim eu Qui</h3>
               <span className="meta">Ted Simmmons June 21, 2015</span>
-              <p className="desc">
+              <p className={styles.desc}>
                 Amet ipsum occaecat aliqua aute nisi laboris nostrud culpa est do. Aliqua esse velit
                 in excepteur esse qui voluptate laboris adipisicing qui irure elit amet excepteur.
               </p>
@@ -75,7 +71,7 @@ export const AllArticles = () => {
               <span className="subtitle">NATURE</span>
               <h3 className="title">Tempor deserunt Sunt Qui</h3>
               <span className="meta">Connie Robertson November 3, 2012</span>
-              <p className="desc">
+              <p className={styles.desc}>
                 Ea qui dolor aute cupidatat ad pariatur proident. Mollit nulla tempor aute
                 reprehenderit ut dolore mollit nisi consequat excepteur ex officia pariatur irure.
               </p>
@@ -89,7 +85,7 @@ export const AllArticles = () => {
               <span className="subtitle">PLANTS</span>
               <h3 className="title">Occaecat Anim eu Qui</h3>
               <span className="meta">Eleanor Williamson September 28, 2014</span>
-              <p className="desc">
+              <p className={styles.desc}>
                 Qui ipsum consectetur ad incididunt et aliquip exercitation laboris nisi
                 reprehenderit. Et excepteur commodo esse enim ea aliqua officia reprehenderit.
               </p>
@@ -103,7 +99,7 @@ export const AllArticles = () => {
               <span className="subtitle">FOOD</span>
               <h3 className="title">Fugiat Incididunt Voluptate Consequat</h3>
               <span className="meta">Bessie Watson June 13, 2012</span>
-              <p className="desc">Mollit ea culpa ipsum pariatur eiusmod. Irure et.</p>
+              <p className={styles.desc}>Mollit ea culpa ipsum pariatur eiusmod. Irure et.</p>
             </div>
           </div>
           {/*  */}
@@ -114,7 +110,7 @@ export const AllArticles = () => {
               <span className="subtitle">COLORFUL</span>
               <h3 className="title">Occaecat Anim eu Qui</h3>
               <span className="meta">Ted Simmmons June 21, 2015</span>
-              <p className="desc">
+              <p className={styles.desc}>
                 Amet ipsum occaecat aliqua aute nisi laboris nostrud culpa est do. Aliqua esse velit
                 in excepteur esse qui voluptate laboris adipisicing qui irure elit amet excepteur.
               </p>
--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,10 +1,49 @@
-import { AllArticles } from "../components/AllArticles/AllArticles";
+import { AllArticles } from '../components/AllArticles/AllArticles';
 
+const tags = [
+  'Technology',
+  'Open Source',
+  'JavaScript',
+  'Minimalism',
+  'Self-help',
+  'Animals',
+  'Herbivores',
+  'HTML',
+  'CSS',
+  'PHP',
+  'Web Technologies',
+  'Career',
+  'Life',
+  'Spirituality',
+  'Food',
+  'Cooking',
+  'Sports',
+  'Racing',
+  'Mountain Hiking',
+  'Cruising',
+];
 
 export const Articles = () => {
   return (
-    <>
-      <AllArticles/>
-    </>
+    <div>
+      <div className="heading-box">
+        <h2 className="heading">All Articles</h2>
+        <div className="heading-line"></div>
+      </div>
+      {/*  */}
+      <div className='main-article'>
+        <AllArticles />
+        {/* tags */}
+        <div className='tags-box'>
+          <h4 className='tags'>tags.</h4>
+          <ul>
+            {tags.map((item, index) => (
+              <li className='tags-item' key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {/* pagination */}
+    </div>
   );
-}
+};
